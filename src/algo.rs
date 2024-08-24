05ad7a4f26a7bcf457d4224f3f275af866a6147f
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-08-24 14:40:40 +0200

    keep pushing

--- src/algo.rs
+++ src/algo.rs
@@ -5,6 +5,7 @@
 // 4) take volume into account when calculating profit and best size (flash loans might be an
 //    option)
 use raydium_amm::math::{Calculator, CheckedCeilDiv, SwapDirection, U128};
+use solana_client::nonblocking::rpc_client::RpcClient;
 use std::collections::HashMap;
 use std::str::FromStr;
 use std::sync::Arc;
@@ -18,8 +19,14 @@ use solana_sdk::pubkey::Pubkey;
 use solana_sdk::transaction::VersionedTransaction;
 use tokio::sync::mpsc;
 
-use crate::raydium::{self, ParsedAmmInstruction};
-use raydium_library::amm::{AmmKeys, CalculateResult};
+use crate::raydium::{self, download_raydium_json, ParsedAmmInstruction};
+use raydium_library::amm::{self, openbook, AmmKeys, CalculateResult};
+
+pub const WHIRLPOOL: &str = "whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc";
+pub const RAYDIUM_CP: &str = "CPMMoo8L3F4NbTegBCKVNunggL7H1ZpdTHKxQB5qKP1C";
+pub const RAYDIUM_AMM: &str = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
+pub const RAYDIUM_LIQUIDITY_POOL_V4_PUBKEY: &str =
+    "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
 
 #[derive(Debug, Default)]
 pub struct PoolsState {
@@ -27,7 +34,11 @@ pub struct PoolsState {
     pub raydium_amm_count: u64,
     pub orca_count: u64,
     pub orca_token_to_pool: HashMap<Pubkey, Arc<OrcaPool>>,
+    // program_id to pool
     pub raydium_pools: HashMap<Pubkey, Arc<RwLock<RaydiumAmmPool>>>,
+    // mint to program_id vec
+    pub raydium_pools_by_mint: HashMap<Pubkey, Vec<Pubkey>>,
+    pub raydium_pool_ids: Vec<Pubkey>,
 }
 
 #[derive(Debug, Default)]
@@ -37,9 +48,19 @@ pub struct OrcaPool {}
 #[derive(Debug, Clone)]
 pub struct RaydiumAmmPool {
     pub token: Pubkey,
-    pub program_id: Pubkey,
     pub amm_keys: AmmKeys,
     pub state: CalculateResult,
+}
+
+pub fn calculate_price(state: &CalculateResult) -> f64 {
+    let pc_amount = state.pool_pc_vault_amount as f64;
+    let coin_amount = state.pool_coin_vault_amount as f64;
+
+    if coin_amount == 0.0 {
+        return 0.0; // Avoid division by zero
+    }
+
+    pc_amount / coin_amount
 }
 
 impl PoolsState {
@@ -70,10 +91,7 @@ impl PoolsState {
                         .await;
                     }
                     Err(e) => {
-                        println!(
-                            "Error parsing instruction {}: {:?}",
-                            idx, e
-                        );
+                        warn!("Error parsing instruction {}: {:?}", idx, e);
                     }
                 }
             }
@@ -133,10 +151,9 @@ impl PoolsState {
                 .await;
             }
             // Handle other instruction types...
-            _ => println!(
-                "Unhandled instruction type: {:?}",
-                parsed_instruction
-            ),
+            _ => {
+                warn!("Unhandled instruction type: {:?}", parsed_instruction)
+            }
         }
     }
 
@@ -152,6 +169,10 @@ impl PoolsState {
         is_swap_base_in: bool,
     ) {
         if let Some(pool) = self.raydium_pools.get(amm_id) {
+            info!(
+                "Initial price: {}",
+                calculate_price(&pool.read().await.state)
+            );
             let mut pool = pool.write().await;
             // double check here
             let is_coin_to_pc = pool.amm_keys.amm_coin_vault
@@ -261,6 +282,8 @@ impl PoolsState {
             // Store or use the calculate_result as needed
             self.raydium_pools.get(amm_id).unwrap().write().await.state =
                 calculate_result;
+
+            info!("Updated price: {}", calculate_price(&pool.state));
         } else {
             warn!("Pool not found for AMM ID: {}", amm_id);
         }
@@ -274,17 +297,39 @@ impl PoolsState {
     }
 }
 
-pub const WHIRLPOOL: &str = "whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc";
-pub const RAYDIUM_CP: &str = "CPMMoo8L3F4NbTegBCKVNunggL7H1ZpdTHKxQB5qKP1C";
-pub const RAYDIUM_AMM: &str = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
-
 pub async fn receive_entries(
     mut entry_receiver: mpsc::Receiver<Vec<Entry>>,
     mut error_receiver: mpsc::Receiver<String>,
 ) {
     let mut pools_state = PoolsState::default();
+    let mints_of_interest = [
+        "3S8qX1MsMqRbiwKg2cQyx7nis1oHMgaCuc9c4VfvVdPN", // mother
+        "3B5wuUrMEi5yATD7on46hKfej3pfmd7t1RKgrsN3pump", // billy
+        "CTg3ZgYx79zrE1MteDVkmkcGniiFrK1hJ6yiabropump", // neiro
+        "GiG7Hr61RVm4CSUxJmgiCoySFQtdiwxtqf64MsRppump", // scf
+        "EbZh3FDVcgnLNbh1ooatcDL1RCRhBgTKirFKNoGPpump", // gringo
+        "GYKmdfcUmZVrqfcH1g579BGjuzSRijj3LBuwv79rpump", // wdog
+        "8Ki8DpuWNxu9VsS3kQbarsCWMcFGWkzzA8pUPto9zBd5", // lockin
+    ]
+    .iter()
+    .map(|p| Pubkey::from_str(p).unwrap())
+    .collect::<Vec<_>>();
 
-    initialize_raydium_amm_pools(&mut pools_state);
+    // TODO use nice rpc, possibly geyser at later stage
+    let rpc_client =
+        RpcClient::new("https://api.mainnet-beta.solana.com".to_string());
+
+    initialize_raydium_amm_pools(
+        &rpc_client,
+        &mut pools_state,
+        mints_of_interest,
+    )
+    .await;
+
+    println!(
+        "Initialized Raydium AMM pools: {}",
+        pools_state.raydium_pools.len()
+    );
 
     tokio::spawn(async move {
         loop {
@@ -300,16 +345,58 @@ pub async fn receive_entries(
     });
 }
 
-fn initialize_raydium_amm_pools(_pools_state: &mut PoolsState) {
-    // TODO: Initialize Raydium AMM pools with their details
-    // This should populate the raydium_pools HashMap
-    // Example:
-    // let pool = RaydiumAmmPool {
-    //     id: Pubkey::from_str("...").unwrap(),
-    //     base_mint: Pubkey::from_str("...").unwrap(),
-    //     // ... initialize other fields
-    // };
-    // pools_state.raydium_pools.insert(pool.id, Arc::new(RwLock::new(pool)));
+pub async fn initialize_raydium_amm_pools(
+    rpc_client: &RpcClient,
+    pools_state: &mut PoolsState,
+    mints_of_interest: Vec<Pubkey>,
+) {
+    download_raydium_json(false)
+        .await
+        .expect("download raydium");
+    let jsonstr = std::fs::read_to_string("raydium.json")
+        .expect("Failed to read raydium.json");
+    let amm_keys_map = raydium::parse_raydium_json(
+        Arc::new(jsonstr),
+        mints_of_interest.clone(),
+    )
+    .expect("parse raydium json");
+    let amm_program =
+        Pubkey::from_str(RAYDIUM_LIQUIDITY_POOL_V4_PUBKEY).expect("pubkey");
+    for mint in mints_of_interest.iter() {
+        let amm_keys_vec = amm_keys_map.get(mint).unwrap(); // bound to exist
+        for amm_keys in amm_keys_vec.iter() {
+            let market_keys = openbook::get_keys_for_market(
+                rpc_client,
+                &amm_keys.market_program,
+                &amm_keys.market,
+            )
+            .await
+            .expect("get market keys");
+            let state = amm::calculate_pool_vault_amounts(
+                rpc_client,
+                &amm_program,
+                &amm_keys.amm_pool,
+                amm_keys,
+                &market_keys,
+                amm::utils::CalculateMethod::CalculateWithLoadAccount,
+            )
+            .await
+            .expect("calculate pool vault amounts");
+            pools_state.raydium_pools.insert(
+                amm_keys.amm_pool,
+                Arc::new(RwLock::new(RaydiumAmmPool {
+                    token: *mint,
+                    amm_keys: *amm_keys,
+                    state,
+                })),
+            );
+            pools_state
+                .raydium_pools_by_mint
+                .entry(*mint)
+                .or_default()
+                .push(amm_keys.amm_pool);
+        }
+    }
 }
 
 pub async fn process_entries_batch(
--- src/raydium.rs
+++ src/raydium.rs
@@ -1,3 +1,17 @@
+use futures_util::StreamExt;
+use indicatif::{ProgressBar, ProgressStyle};
+use log::{info, warn};
+use raydium_library::amm::AmmKeys;
+use reqwest::Client;
+use serde_json::Value;
+use solana_sdk::pubkey::Pubkey;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::str::FromStr;
+use std::sync::Arc;
+
 use raydium_amm::instruction::{
     AdminCancelOrdersInstruction, ConfigArgs, DepositInstruction,
     InitializeInstruction, InitializeInstruction2, MonitorStepInstruction,
@@ -93,5 +107,116 @@ fn unpack_u64(input: &[u8]) -> Result<(u64, &[u8]), ProgramError> {
         Ok((amount, rest))
     } else {
         Err(ProgramError::InvalidInstructionData)
+    }
+}
+
+pub async fn download_raydium_json(
+    update: bool,
+) -> Result<(), Box<dyn std::error::Error>> {
+    if Path::new("raydium.json").exists() && !update {
+        warn!("raydium.json already exists. Skipping download.");
+        return Ok(());
+    }
+    info!("Downloading raydium.json");
+
+    let url = "https://api.raydium.io/v2/sdk/liquidity/mainnet.json";
+    let client = Client::new();
+    let res = client.get(url).send().await?;
+    let total_size = res.content_length().unwrap_or(0);
+
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+        .unwrap()
+        .progress_chars("#>-"));
+
+    let mut file = File::create("raydium.json")?;
+    let mut downloaded: u64 = 0;
+    let mut stream = res.bytes_stream();
+
+    while let Some(item) = stream.next().await {
+        let chunk = item?;
+        file.write_all(&chunk)?;
+        let new =
+            std::cmp::min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        pb.set_position(new);
+    }
+
+    pb.finish_with_message("Download completed");
+    Ok(())
+}
+
+// this takes long, possibly could make it so that it uses a search index it
+// returns all of the
+// pools for a given token, to be filtered later for relevant
+pub fn parse_raydium_json(
+    jsonstr: Arc<String>,
+    mints_of_interest: Vec<Pubkey>,
+) -> Result<HashMap<Pubkey, Vec<AmmKeys>>, Box<dyn std::error::Error>> {
+    let mut result = HashMap::new();
+    let all: Value = serde_json::from_str(&jsonstr).unwrap();
+    info!("Parsing relevant pools");
+
+    let pools = all["unOfficial"].as_array().unwrap();
+    let total_pools = pools.len();
+
+    let pb = ProgressBar::new(total_pools as u64);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})")
+        .unwrap()
+        .progress_chars("#>-"));
+
+    for (index, json) in pools.iter().enumerate() {
+        let base_mint =
+            Pubkey::from_str(json["baseMint"].as_str().unwrap()).unwrap();
+        let quote_mint =
+            Pubkey::from_str(json["quoteMint"].as_str().unwrap()).unwrap();
+        if mints_of_interest.contains(&base_mint) {
+            let amm_keys = json_to_amm_keys(json);
+            result.entry(base_mint).or_insert(vec![]).push(amm_keys);
+        } else if mints_of_interest.contains(&quote_mint) {
+            let amm_keys = json_to_amm_keys(json);
+            result.entry(quote_mint).or_insert(vec![]).push(amm_keys);
+        }
+
+        pb.set_position((index + 1) as u64);
+    }
+
+    pb.finish_with_message("Parsing completed");
+
+    if mints_of_interest.len() != result.len() {
+        warn!("Not all mints found in raydium.json");
+    }
+    Ok(result)
+}
+
+pub fn json_to_amm_keys(json: &Value) -> AmmKeys {
+    AmmKeys {
+        amm_pool: Pubkey::from_str(json["id"].as_str().unwrap()).unwrap(),
+        amm_coin_mint: Pubkey::from_str(json["baseMint"].as_str().unwrap())
+            .unwrap(),
+        amm_pc_mint: Pubkey::from_str(json["quoteMint"].as_str().unwrap())
+            .unwrap(),
+        amm_authority: Pubkey::from_str(json["authority"].as_str().unwrap())
+            .unwrap(),
+        amm_target: Pubkey::from_str(json["targetOrders"].as_str().unwrap())
+            .unwrap(),
+        amm_coin_vault: Pubkey::from_str(json["baseVault"].as_str().unwrap())
+            .unwrap(),
+        amm_pc_vault: Pubkey::from_str(json["quoteVault"].as_str().unwrap())
+            .unwrap(),
+        amm_lp_mint: Pubkey::from_str(json["lpMint"].as_str().unwrap())
+            .unwrap(),
+        amm_open_order: Pubkey::from_str(
+            json["openOrders"].as_str().unwrap(),
+        )
+        .unwrap(),
+        market_program: Pubkey::from_str(
+            json["marketProgramId"].as_str().unwrap(),
+        )
+        .unwrap(),
+        market: Pubkey::from_str(json["marketId"].as_str().unwrap()).unwrap(),
+        nonce: u8::default(), // not relevant
     }
 }
