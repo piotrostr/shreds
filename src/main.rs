880f651f86e4e7d361038a3006731f3f936eb120
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-08-24 16:40:37 +0200

    fix download

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::{App, Arg};
 use log::info;
 use shreds::listener;
+use shreds::raydium::download_raydium_json;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -25,6 +26,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .help("Run in save mode (dump packets to file)")
                 .takes_value(false),
         )
+        .arg(
+            Arg::with_name("download")
+                .short('d')
+                .long("download")
+                .takes_value(false),
+        )
         .get_matches();
 
     env_logger::Builder::default()
@@ -34,6 +41,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let bind_addr = matches.value_of("bind").unwrap();
     info!("Binding to address: {}", bind_addr);
+
+    if matches.is_present("download") {
+        download_raydium_json(true).await?;
+        return Ok(());
+    }
 
     if matches.is_present("save") {
         info!("Running in save mode");
