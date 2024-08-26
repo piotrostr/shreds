ad5057a2073b106871ac081d20b8f44769e40ed4
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-08-26 13:40:45 +0200

    always log to the same file

--- src/logger.rs
+++ src/logger.rs
@@ -2,8 +2,9 @@ use std::fs::File;
 use std::io::Read;
 
 pub fn setup() -> Result<(), Box<dyn std::error::Error>> {
-    let random = grab_random_bytes();
-    let log_file = File::create(format!("logs/{}.log", hex::encode(random)))?;
+    // let random = grab_random_bytes();
+    // let log_file = File::create(format!("shreds-{}.log", hex::encode(random)))?;
+    let log_file = File::create("shreds.log")?;
     println!("Logging to: {:?}", log_file);
     env_logger::Builder::default()
         .format_module_path(false)
@@ -15,7 +16,7 @@ pub fn setup() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn grab_random_bytes() -> [u8; 5] {
+pub fn grab_random_bytes() -> [u8; 5] {
     let mut random = [0u8; 5];
     File::open("/dev/urandom")
         .expect("asdf")
